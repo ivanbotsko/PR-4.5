0f8649ab38f12f8203d5070fc4e6b6c155556ac6
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-10-10 19:22:02 +0300

    first master commit

---
+++ PR 4.5/PR 4.5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		if ((y * y <= R * R - x * x && y >= 0) ||
+			(y <= x && y >= -R && y <= 0 && x <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+	for (int i = 0; i < 10; i++)
+	{
+		x = 6. * rand() / RAND_MAX - 3;
+		y = 6. * rand() / RAND_MAX - 3;
+		R== 6. * rand() / RAND_MAX - 3;
+		if ((y * y <= R * R - x * x && y >= 0) ||
+			(y <= x && y >= -R && y <= 0 && x <= 0))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << setw(8) << setprecision(4) << R << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << setw(8) << setprecision(4) << R << " " << "no" << endl;
+	}
+	return 0;
+}
